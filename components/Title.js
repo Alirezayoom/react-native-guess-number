cee38bb83f7b1abe4cbd7abcde85b2d5808d66d2
alirezayoom <alirezayoom@gmail.com>
2024-04-20 19:55:21 +0430

    add title

---
+++ components/Title.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text } from "react-native";
+
+function Title({ children }) {
+  return <Text style={styles.title}>{children}</Text>;
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    borderColor: "black",
+    borderWidth: 2,
+    padding: 8,
+    textAlign: "center",
+  },
+});
+
+export default Title;
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,11 +1,26 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import Title from "../components/Title";
 
 function GameScreen() {
   return (
-    <View>
-      <Text>game screen</Text>
+    <View style={styles.screen}>
+      <Title>Opponents Guess</Title>
+
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* increase and decrease */}
+      </View>
+
+      <View>{/* Logs */}</View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
 
 export default GameScreen;
