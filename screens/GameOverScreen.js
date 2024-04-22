f16b150dd313c88f356402b33eb4d7d52fdbc126
alirezayoom <alirezayoom@gmail.com>
2024-04-22 23:16:37 +0430

    add style to game over screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,11 +1,38 @@
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import Title from "../components/ui/Title";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 function GameOverScreen() {
   return (
-    <View>
-      <Text>game over!</Text>
+    <View style={styles.rootContainer}>
+      <Title>Game Over!</Title>
+      <View>
+        <Image source={require("../assets/images/game-over.png")} />
+      </View>
+      <Text style={styles.textSummary}>
+        Your phone needed <Text style={styles.highlight}>X</Text> rounds to
+        guess the number <Text style={styles.highlight}>Y</Text>.
+      </Text>
+      <PrimaryButton>Start New Game</PrimaryButton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textSummary: {
+    fontFamily: "poppins",
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  highlight: {
+    fontFamily: "poppins-bold",
+  },
+});
 
 export default GameOverScreen;
